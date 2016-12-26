0b6ffa45945eb2c25d807a82d380991379873f8b
Chris Ro <christopher.j.ro@gmail.com>
2016-12-25 22:38:59 -0800

    Add QuickNameAccess class

---
+++ quick_name_accessor.py
@@ -0,0 +1,16 @@
+class QuickNameAccess(object):
+  '''Allows quick name lookups to get related box numbers'''
+
+  def __init__(self):
+    pass
+
+  def find_matches_by_name(self, name):
+    '''Given a name to identify the boxholder, returns best box matches
+
+    Args:
+      name: string of name to identify boxholder
+
+    Returns:
+      List of box numbers as strings, in descending order of match strength
+    '''
+    pass
