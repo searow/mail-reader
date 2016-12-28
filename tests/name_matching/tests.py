c7b46b0ccf224be243ff56b1b1878d0b737e543a
Chris Ro <christopher.j.ro@gmail.com>
2016-12-28 00:11:30 -0800

    Add test code and skeleton tests for name_matching

---
+++ tests/name_matching/tests.py
@@ -0,0 +1,35 @@
+import unittest
+
+class TestQuickNameAccessor(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def test_perfect_match_name_only_one_box(self):
+    pass
+
+  def test_perfect_match_name_multiple_boxes(self):
+    pass
+
+  def test_similar_match_only_one_box(self):
+    pass
+
+  def test_similar_match_multiple_boxes(self):
+    pass
+
+  def test_no_perfect_match(self):
+    pass
+
+  def test_no_remotely_close_match(self):
+    pass
+
+  def test_kinda_close_match_one_box(self):
+    pass
+
+  def test_kinda_close_match_multiple_boxes(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
