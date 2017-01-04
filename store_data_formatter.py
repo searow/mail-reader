6b50ded859e30d6f0d5673dde90e37d6bb567dad
Chris Ro <christopher.j.ro@gmail.com>
2017-01-03 21:10:41 -0800

    Add format_next_customer method and helpers

--- store_data_formatter.py
+++ store_data_formatter.py
@@ -1,5 +1,6 @@
 import abc
 import pandas as pd
+import re
 
 class StoreDataFormatter(metaclass=abc.ABCMeta):
   '''Abstract class to format existing customer data 
@@ -38,6 +39,46 @@ class BAPDataFormatter(StoreDataFormatter):
     df = pd.read_excel(fname)
     headers = ['NAME', 'SUITE']
     self.data = df[headers]
+    self.data_len = super().get_data_len()
+    self.current_import_idx = 0
 
   def format_next_customer(self):
-    pass
+    '''Returns formatted customer names and box numbers
+
+    Reads the next customer from the data. Splits up the line items into 
+    standard data format that all StoreDataFormatters will use. Returns this 
+    standard data format for use by the actual in-memory storage object.
+
+    Returns:
+      Nested list of names and suites: [['NAME0', SUITE#],['NAME1', SUITE#],...]
+    '''
+
+    line = self.data.iloc[self.current_import_idx]
+    self.current_import_idx += 1
+
+    # Name splitting occurs here
+    customers = []
+    name = line['NAME']
+    suite = line['SUITE']
+    clean_name = self._remove_redundant_identifiers(name)
+    name_split = self._split_name_remove_empty(clean_name)
+    for item in name_split:
+      customers.append([item, suite])
+
+    return customers 
+
+  def _split_name_remove_empty(self, name):
+    delimiters = r'[, ]'
+    name = name.replace('.','')
+    name_split = list(filter(None, re.split(delimiters, name)))
+
+    return name_split
+
+  def _remove_redundant_identifiers(self, name):
+    '''Removes less useful words, such as INC, MR, MRS, JR, CO'''
+    # TODO(searow): need to implement this
+    return name
+
+  def has_next_customer(self):
+    '''Checks if there are more customers to format'''
+    return self.current_import_idx < self.data_len
