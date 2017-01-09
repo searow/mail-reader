dc254d68d256a01c41f469acd1fe00f46097ed0d
Chris Ro <christopher.j.ro@gmail.com>
2017-01-09 13:04:13 -0800

    Switch setup to use DataFormatter and QuickNameAccess

--- tests/name_matching/tests.py
+++ tests/name_matching/tests.py
@@ -1,17 +1,24 @@
 import unittest
 import pandas as pd
+import quick_name_accessor as qna
+import store_data_formatter as sdf
 
 class TestQuickNameAccessor(unittest.TestCase):
   def setUp(self):
     # TODO(searow): consider switching this to setUpClass for performance 
     fname = './tests/name_matching/name_to_box_number.xlsx'
-    df = pd.read_excel(fname)
-    headers = ['NAME', 'BOX']
-    self.data = df[headers]
+    formatter = sdf.BAPDataFormatter(fname)
+    self.name_accessor = qna.QuickNameAccess()
+    self.name_accessor.create_entries_with_formatter(formatter)
+
+  def test_add_name(self):
+    pass
 
   def test_perfect_match_name_only_one_box(self):
     # NAILSALON, 101
-    pass
+    names = ['NAILSALON']
+    boxes = self.name_accessor.find_matches_by_names(names)
+    self.assertEqual(101, boxes[0])
 
   def test_perfect_match_name_multiple_boxes(self):
     # FOX MCCLOUD, 333/555
