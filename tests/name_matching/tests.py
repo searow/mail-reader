355dc4c1a52a2d8781ca82a3dd0b1cfeedb3dd44
Chris Ro <christopher.j.ro@gmail.com>
2017-01-11 20:48:31 -0800

    Add tests for each test case

--- tests/name_matching/tests.py
+++ tests/name_matching/tests.py
@@ -18,36 +18,59 @@ class TestQuickNameAccessor(unittest.TestCase):
     # NAILSALON, 101
     names = ['NAILSALON']
     boxes = self.name_accessor.find_matches_by_names(names)
-    self.assertEqual(101, boxes[0])
+    self.assertEqual(101, boxes[0][0])
 
   def test_perfect_match_name_multiple_boxes(self):
     # FOX MCCLOUD, 333/555
     # SLIPPY TOAD, 333/444
-    pass
+    names = ['FOX', 'MCCLOUD']
+    boxes = self.name_accessor.find_matches_by_names(names)
+    boxes = boxes[0:2]
+    self.assertTrue(any(333 in item for item in boxes)) 
+    self.assertTrue(any(555 in item for item in boxes)) 
+
+    names = ['SLIPPY', 'TOAD']
+    boxes = self.name_accessor.find_matches_by_names(names)
+    boxes = boxes[0:2]
+    self.assertTrue(any(333 in item for item in boxes)) 
+    self.assertTrue(any(444 in item for item in boxes)) 
 
   def test_decent_match_only_one_box(self):
     # N4ILSALOON, 101
-    pass
+    names = ['N4ILSALOON']
+    boxes = self.name_accessor.find_matches_by_names(names)
+    self.assertEqual(101, boxes[0][0])
 
   def test_decent_match_multiple_boxes(self):
     # MARCUS MCOLOUD, 333/555
-    pass
+    names = ['MARCUS', 'MCOLOUD']
+    boxes = self.name_accessor.find_matches_by_names(names)
+    self.assertTrue(any(333 in item for item in boxes))
+    self.assertTrue(any(555 in item for item in boxes))
 
   def test_match_only_commons(self):
     # CLIFFORD THE BIG RED DOG INC, ???
+    # TODO(searow): need to add bad matches handling and no matching letter
+    #               exception handling for this one
     pass
 
   def test_no_remotely_close_match(self):
     # PEPPY HARE, ???
+    # TODO(searow): need to add bad matches handling and no matching letter
+    #               exception handling for this one
     pass
 
   def test_improperly_split_name(self):
     # FALCO LOM BARDI, 333
-    pass
+    names = ['FALCO', 'LOM', 'BARDI']
+    boxes = self.name_accessor.find_matches_by_names(names)
+    self.assertEqual(333, boxes[0][0])
 
   def test_improperly_appended_names(self):
     # JAMESMCCLOUD, 555
-    pass
+    names = ['JAMESMCCLOUD']
+    boxes = self.name_accessor.find_matches_by_names(names)
+    self.assertEqual(555, boxes[0][0])
 
   def tearDown(self):
     pass
