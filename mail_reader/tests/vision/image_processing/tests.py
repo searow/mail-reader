7c6a84ba716a753ab0b074c058490e7f24f717c5
Chris Ro <christopher.j.ro@gmail.com>
2017-02-02 09:01:33 -0800

    Move img defn to test setup and switch to dict

--- mail_reader/tests/vision/image_processing/tests.py
+++ mail_reader/tests/vision/image_processing/tests.py
@@ -7,22 +7,30 @@ class TestImageProcessing(unittest.TestCase):
   def setUp(self):
     ocr = ocr_processor.TesseractProcessor()
     self.processor = image_processor.ImageProcessor(ocr)
-    pass
+
+    # Append each test image that we want to test here
+    self.images = [] 
+    self.images.append({
+      'path' : './mail_reader/tests/vision/image_processing/test_image_0.jpg',
+      'rotation_ans' : 0.97,
+      'roi_ans' : (695, 324)
+      })
+    self.images.append({
+      'path' : './mail_reader/tests/vision/image_processing/test_image_1.jpg',
+      'rotation_ans' : -6.46,
+      'roi_ans' : (600, 429)
+      })
 
   def test_calculate_rotation(self):
     # Maximum +- error allowed in degrees
     error = 1
 
-    # Append each image that we want to test here
-    images = []  # [filepath, answer]
-    images.append(['./mail_reader/tests/vision/image_processing/test_image_0_97.jpg', 0.97])
-    images.append(['./mail_reader/tests/vision/image_processing/test_image_6_46.jpg', -6.46])
-
-    for [path, ans] in images:
-      img = cv2.cvtColor(cv2.imread(path), cv2.COLOR_RGB2GRAY)
+    for data in self.images:
+      img = cv2.cvtColor(cv2.imread(data['path']), cv2.COLOR_RGB2GRAY)
       self.processor.original_image = img
       rotation = self.processor._calculate_rotation()
-      self.assertTrue(ans - error < rotation < ans + error)
+      answer = data['rotation_ans']
+      self.assertTrue(answer - error < rotation < answer + error)
 
   def test_center_region_of_interest(self):
     # Success is based on point of center of area of interest. Must be within
