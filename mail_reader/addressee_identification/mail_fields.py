a5e30604344a68bbea3c773e1ee195c5fd9bc906
Chris Ro <christopher.j.ro@gmail.com>
2017-02-08 14:01:33 -0800

    Populate MailFields attributes

--- mail_reader/addressee_identification/mail_fields.py
+++ mail_reader/addressee_identification/mail_fields.py
@@ -8,5 +8,25 @@ class MailFields(object):
   pass between processing objects that represents potential identifiers in the
   addressee section. TextAnalyzer is responsible for populating these fields.
   """
+  self.addressee_line = {
+      'prefix': None,  # Mr. Mrs. Dr.
+      'first_name': None,
+      'last_name': None,
+      'suffix': None,  # Jr. Sr. III Esq
+      'all_names': None,  # All names as list
+      'secondary_entity': None  # Alternate person or name
+  }
+  self.probable_box = None
+  self.street_line = {
+      'number': None,
+      'street_name': None,
+      'secondary_id': None,  # APT, SUITE, ETC
+  }
+  self.city_line = {
+      'city': None,
+      'state': None,
+      'zip_code': None  # 5 digit zip only
+  }
+
   def __init__(self):
     pass
