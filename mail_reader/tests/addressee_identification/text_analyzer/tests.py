5caa73a19fafee8ed20383ebb27728fdc7d743d7
Chris Ro <christopher.j.ro@gmail.com>
2017-02-14 01:44:39 -0800

    Change tests to mimic new structure with usaddress lib

--- mail_reader/tests/addressee_identification/text_analyzer/tests.py
+++ mail_reader/tests/addressee_identification/text_analyzer/tests.py
@@ -7,34 +7,34 @@ class TestTextAnalyzer(unittest.TestCase):
 
   def test_simple_address(self):
     lines = [
-      ['FOX MCCLOUD'],
-      ['444 GREAT FOX CABIN DR'],
-      ['CORNERIA, CA 90000']
+      'FOX MCCLOUD',
+      '444 GREAT FOX CABIN DR',
+      'CORNERIA, CA 90000'
     ]
     fields = self.analyzer.parse_text_lines(lines)
-    self.assertEqual(fields.addressee_line['first_name'], 'FOX')
-    self.assertEqual(fields.addressee_line['last_name'], 'MCCLOUD')
-    self.assertEqual(fields.street_line['number'], "444")
-    self.assertEqual(fields.street_line['street_name'], 'GREAT FOX CABIN DR')
-    self.assertEqual(fields.city_line['city'], 'CORNERIA')
-    self.assertEqual(fields.city_line['state'], 'CA')
-    self.assertEqual(fields.city_line['zip_code'], 90000)
-    
+    self.assertTrue('FOX' in fields.addressee_line['all_names'])
+    self.assertTrue('MCCLOUD' in fields.addressee_line['all_names'])
+    self.assertTrue('444' in fields.street_line['number'])
+    self.assertTrue('GREAT FOX CABIN' in fields.street_line['street_name'])
+    self.assertTrue('CORNERIA' in fields.city_line['city'])
+    self.assertTrue('CA' in fields.city_line['state'])
+    self.assertTrue('90000' in fields.city_line['zip_code'])
+
   def test_actual_address(self):
     lines = [
-      ['DR ALBERT EINSTEIN'],
-      ['112 MERCER STREET'],
-      ['PRINCETON, NJ 08540']
+      'DR ALBERT EINSTEIN',
+      '112 MERCER STREET',
+      'PRINCETON, NJ 08540'
     ]
     fields = self.analyzer.parse_text_lines(lines)
-    self.assertEqual(fields.addressee_line['prefix'], 'DR')
-    self.assertEqual(fields.addressee_line['first_name'], 'ALBERT')
-    self.assertEqual(fields.addressee_line['last_name'], 'EINSTEIN')
-    self.assertEqual(fields.street_line['number'], 112)
-    self.assertEqual(fields.street_line['street_name'], 'MERCER STREET')
-    self.assertEqual(fields.city_line['city'], 'PRINCETON')
-    self.assertEqual(fields.city_line['state'], 'NJ')
-    self.assertEqual(fields.city_line['zip_code'], 8540)
+    self.assertTrue('DR' in fields.addressee_line['all_names'])
+    self.assertTrue('ALBERT' in fields.addressee_line['all_names'])
+    self.assertTrue('EINSTEIN' in fields.addressee_line['all_names'])
+    self.assertTrue('112' in fields.street_line['number'])
+    self.assertTrue('MERCER' in fields.street_line['street_name'])
+    self.assertTrue('PRINCETON' in fields.city_line['city'])
+    self.assertTrue('NJ' in fields.city_line['state'])
+    self.assertTrue('08540' in fields.city_line['zip_code'])
 
   def test_variant_box_separate_line(self):
     box_names = ['PMB', 'BOX', '#', 'SUITE', 'PO BOX', 'POBOX', 'PO', '',
@@ -44,26 +44,27 @@ class TestTextAnalyzer(unittest.TestCase):
       box_names[idx] = name + '102'
 
     lines = [
-      ['RONALD MCDONALD JR'],
-      [''],
-      ['111 1/2 MEMORY LANE'],
-      ['SAN FRANCISCO, CA 94101']
+      'RONALD MCDONALD JR',
+      '',
+      '111 1/2 MEMORY LANE',
+      'SAN FRANCISCO, CA 94101'
     ]
-    fields = self.analyzer.parse_text_lines(lines)
 
     # Test out each box name variant
     for name in box_names:
-      lines[1] = name
-      self.assertEqual(fields.addressee_line['first_name'], 'RONALD')
-      self.assertEqual(fields.addressee_line['last_name'], 'MCDONALD')
-      self.assertEqual(fields.addressee_line['suffix'], 'JR')
-      self.assertEqual(fields.street_line['number'], "111 1/2")
-      self.assertEqual(fields.street_line['street_name'], 'MEMORY LANE')
-      self.assertEqual(fields.city_line['city'], 'SAN FRANCISCO')
-      self.assertEqual(fields.city_line['state'], 'CA')
-      self.assertEqual(fields.city_line['zip_code'], 94101)
-      self.assertEqual(fields.probable_box, '102',
-                       'Failed box name type: ' + name)
+      test_lines = lines.copy()
+      test_lines[1] += name
+      fields = self.analyzer.parse_text_lines(test_lines)
+      self.assertTrue('RONALD' in fields.addressee_line['all_names'])
+      self.assertTrue('MCDONALD' in fields.addressee_line['all_names'])
+      self.assertTrue('JR' in fields.addressee_line['all_names'])
+      self.assertTrue('111' in fields.street_line['number'])
+      self.assertTrue('MEMORY' in fields.street_line['street_name'])
+      self.assertTrue('SAN FRANCISCO' in fields.city_line['city'])
+      self.assertTrue('CA' in fields.city_line['state'])
+      self.assertTrue('94101' in fields.city_line['zip_code'])
+      self.assertTrue('102' in fields.probable_box,
+                      'Failed box name type: ' + name)
 
   def test_variant_box_appended_to_street_line(self):
     box_names = ['PMB', 'BOX', '#', 'SUITE', 'PO BOX', 'POBOX', 'PO', '',
@@ -73,26 +74,101 @@ class TestTextAnalyzer(unittest.TestCase):
       box_names[idx] = name + '102'
 
     lines = [
-      ['RONALD MCDONALD JR'],
-      ['111 1/2 MEMORY LANE '],
-      ['SAN FRANCISCO, CA 94101']
+      'RONALD MCDONALD JR',
+      '111 1/2 MEMORY LANE ',
+      'SAN FRANCISCO, CA 94101'
     ]
-    fields = self.analyzer.parse_text_lines(lines)
 
     # Test out each box name variant
     for name in box_names:
       # Append the box name to the 2nd line
-      lines[1] = name + lines[1][0]
-      self.assertEqual(fields.addressee_line['first_name'], 'RONALD')
-      self.assertEqual(fields.addressee_line['last_name'], 'MCDONALD')
-      self.assertEqual(fields.addressee_line['suffix'], 'JR')
-      self.assertEqual(fields.street_line['number'], "111 1/2")
-      self.assertEqual(fields.street_line['street_name'], 'MEMORY LANE')
-      self.assertEqual(fields.city_line['city'], 'SAN FRANCISCO')
-      self.assertEqual(fields.city_line['state'], 'CA')
-      self.assertEqual(fields.city_line['zip_code'], 94101)
-      self.assertEqual(fields.probable_box, '102',
-                       'Failed box name type: ' + name)
+      test_lines = lines.copy()
+      test_lines[1] += name
+      fields = self.analyzer.parse_text_lines(test_lines)
+      self.assertTrue('RONALD' in fields.addressee_line['all_names'])
+      self.assertTrue('MCDONALD' in fields.addressee_line['all_names'])
+      self.assertTrue('JR' in fields.addressee_line['all_names'])
+      self.assertTrue('111' in fields.street_line['number'])
+      self.assertTrue('MEMORY' in fields.street_line['street_name'])
+      self.assertTrue('SAN FRANCISCO' in fields.city_line['city'])
+      self.assertTrue('CA' in fields.city_line['state'])
+      self.assertTrue('94101' in fields.city_line['zip_code'])
+      self.assertTrue('102' in fields.probable_box,
+                      'Failed box name type: ' + name)
+
+  def test_variant_box_separate_line_and_appended_suite(self):
+    box_names = ['PMB', 'BOX', '#', 'SUITE', 'PO BOX', 'POBOX', 'PO', '',
+                 'PMB ', 'BOX ', '# ', 'SUITE ', 'PO BOX ', 'POBOX ', 'PO ']
+    # Append box number to each of them separately
+    for idx, name in enumerate(box_names):
+      box_names[idx] = name + '102'
+
+    lines = [
+      'RONALD MCDONALD JR',
+      '',
+      '111 1/2 MEMORY LANE SUITE 7A',
+      'SAN FRANCISCO, CA 94101'
+    ]
+
+    # Test out each box name variant
+    for name in box_names:
+      # Use test_lines instead to create a fresh version of lines to do the 
+      # appending for the tests
+      test_lines = lines.copy()
+      test_lines[1] += name
+      fields = self.analyzer.parse_text_lines(test_lines)
+      self.assertTrue('RONALD' in fields.addressee_line['all_names'])
+      self.assertTrue('MCDONALD' in fields.addressee_line['all_names'])
+      self.assertTrue('JR' in fields.addressee_line['all_names'])
+      self.assertTrue('111' in fields.street_line['number'])
+      self.assertTrue('MEMORY' in fields.street_line['street_name'])
+      self.assertTrue('SAN FRANCISCO' in fields.city_line['city'])
+      self.assertTrue('CA' in fields.city_line['state'])
+      self.assertTrue('94101' in fields.city_line['zip_code'])
+      self.assertTrue('102' in fields.probable_box,
+                      'Failed box name type: ' + name)
+
+  def test_garbage_in_first_lines(self):
+    lines = [
+      '@||| 1 ((@',
+      'SFHHHHEJ 1231CCC IIII111029',
+      '',
+      'OSCAR THE GROUCH',
+      'TRASH CAN',
+      '123 SESAME STREET',
+      'MANHATTAN, NEW YORK 10010'
+    ]
+
+    fields = self.analyzer.parse_text_lines(lines)
+    self.assertTrue('OSCAR' in fields.addressee_line['all_names'])
+    self.assertTrue('THE' in fields.addressee_line['all_names'])
+    self.assertTrue('GROUCH' in fields.addressee_line['all_names'])
+    self.assertTrue('TRASH' in fields.addressee_line['all_names'])
+    self.assertTrue('CAN' in fields.addressee_line['all_names'])
+    self.assertTrue('123' in fields.street_line['number'])
+    self.assertTrue('SESAME' in fields.street_line['street_name'])
+    self.assertTrue('MANHATTAN' in fields.city_line['city'])
+    self.assertTrue('NEW YORK' in fields.city_line['state'])
+    self.assertTrue('10010' in fields.city_line['zip_code'])
+
+  def test_garbage_in_middle_lines(self):
+    lines = [
+      'MR ALOYSIUS SNUFFLEUPAGUS',
+      '|||! !((@     ',
+      '',
+      '123 SESAME STREET',
+      'MANHATTAN, NEW YORK 10010'
+    ]
+    fields = self.analyzer.parse_text_lines(lines)
+    self.assertTrue('MR' in fields.addressee_line['all_names'])
+    self.assertTrue('ALOYSIUS' in fields.addressee_line['all_names'])
+    self.assertTrue('SNUFFLEUPAGUS' in fields.addressee_line['all_names'])
+    self.assertTrue('123' in fields.street_line['number'])
+    self.assertTrue('SESAME' in fields.street_line['street_name'])
+    self.assertTrue('MANHATTAN' in fields.city_line['city'])
+    self.assertTrue('NEW YORK' in fields.city_line['state'])
+    self.assertTrue('10010' in fields.city_line['zip_code'])
+
 
   def tearDown(self):
     pass
