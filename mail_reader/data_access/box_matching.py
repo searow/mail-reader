8b892cf715b68a28b4af572970246db2b8815505
Chris Ro <christopher.j.ro@gmail.com>
2017-02-16 01:50:25 -0800

    Add _calculate_scores and helpers

--- mail_reader/data_access/box_matching.py
+++ mail_reader/data_access/box_matching.py
@@ -1,3 +1,31 @@
+def _get_box_scores(name_to_match, name_list, box_multiplier):
+  """Calculates box score list for each name in name_list and returns scores.
+
+  Calculates score for each name. Only the highest score for each box is saved
+  and the rest are discarded.
+
+  Args:
+    name_to_match: Name that we are trying to match (ie: 'FOX').
+    name_list: Names that we want to score and their associated box numbers
+               (ie [['FALCO', 555], ['FOX', 111], ['FRIEND', 2], ...]).
+    box_multiplier: List of boxes that should have a multiplied score.
+
+  Returns:
+    Nested list of box number and score [[box, score], [box, score], ...].
+  """
+  pass
+
+def _combine_boxes_scores(box_scores):
+  """Returns overall box score list sorted in decreasing score order.
+
+  Args:
+    box_scores: Nested list of box score for each name that was checked.
+
+  Returns:
+    Overall box score list in decreasing score order.
+  """
+  pass
+
 class BoxMatcher(object):
   """Performs box matching operations to match MailFields with box number.
 
@@ -6,6 +34,7 @@ class BoxMatcher(object):
 
   Attributes:
     __database: Database from which to get box data.
+    __fields: MailFields object passed in by get_matches.
   """
   def __init__(self):
     pass
@@ -30,5 +59,20 @@ class BoxMatcher(object):
 
     Returns:
       None.
+    """
+    pass
+
+  def _calculate_scores(self):
+    """Calculates score for the fields using the database and returns matches.
+
+    Score is calculated using two fields: names in box and probable box. 
+    Gets probable matches for each name and calculates similarity score. Best 
+    similarity score for each box is saved. If box with similarity score is
+    contained in probable box in fields, score is multiplied. Score for each
+    box for each name is combined and overall score for each box is saved.
+
+    Returns:
+      List of box numbers with associated match score: 
+        [[box, score], [box, score], ...]
     """
     pass
