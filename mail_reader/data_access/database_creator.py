e6e5f02993ce78d48e87132a34684fad246f5cab
Chris Ro <christopher.j.ro@gmail.com>
2017-02-17 20:01:00 -0800

    Initial commit database_creator

---
+++ mail_reader/data_access/database_creator.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+class DatabaseCreator(object):
+  """Creates an in-memory sqlite3 database using input data."""
+  def __init__(self):
+    pass
+
+class BapDatabaseCreator(DatabaseCreator):
+  """Subclass to handle specific data files."""
+  def __init__(self):
+    pass
