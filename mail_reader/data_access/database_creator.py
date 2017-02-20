eba91525b7b1698a763703119ed21242200dc3f6
Chris Ro <christopher.j.ro@gmail.com>
2017-02-20 00:28:33 -0800

    Add __populate_file_entries

--- mail_reader/data_access/database_creator.py
+++ mail_reader/data_access/database_creator.py
@@ -1,5 +1,26 @@
 import sqlite3
 import pandas
+import re
+
+def _split_names(raw_name):
+  """Splits name string into list of strings based on delimiter.
+
+  Removes periods (doesn't replace with space), then removes based on 
+  delimiter. Delimiter set to be spaces and commas, specifically to split based
+  on most common delimiters in customer database.
+
+  Args:
+    raw_name: String of name to be split 'THE BE.AR COMPANY, LLC'.
+
+  Returns:
+    List of strings split into parts : ['THE', 'BEAR', 'COMPANY', 'LLC'].
+  """
+  # Remove periods from raw name, no replacement
+  names = raw_name.replace('.','')
+  # Split name by commas and spaces
+  names = filter(None, re.split(r'[, ]', names))
+
+  return names
 
 class DatabaseCreator(object):
   """Creates an in-memory sqlite3 database using input data.
@@ -70,8 +91,62 @@ class DatabaseCreator(object):
     """)
 
   def __populate_file_entries(self):
-    pass
+    """Populates database with data in excel file."""
+    c = self.__db_conn.cursor()
+    active_boxes = self.__determine_active_boxes()
 
+    # Populate box_activities table first since that data is aggregate
+    for box in active_boxes:
+      c.execute('''
+          INSERT INTO box_activities (box_id, active)
+          VALUES (?, ?)
+      ''', (box, active_boxes[box]))
+
+    # Populate the rest of the tables since we have the active_boxes data, 
+    # going row by row and adding items.
+    for idx, row in enumerate(self.__df.iterrows()):
+      key = row[1]
+      # Table entity_statuses
+      c.execute('''
+          INSERT INTO entity_statuses (entity_id, current)
+          VALUES (?, ?);
+      ''', (idx, key['ACTIVE']))
+
+      # Table unique_entity_names
+      # Catch IntegrityError here since we might be adding non-unique 
+      # combinations of entity_id + unique_entity_name. We don't need to do
+      # anything to handle them for now.
+      raw_name = key['NAME']
+      names = _split_names(raw_name)
+      # Insert every name into the database, skipping integrity errors
+      for name in names:
+        try:
+          c.execute('''
+              INSERT INTO unique_entity_names (entity_id, unique_entity_name)
+              VALUES (?, ?);
+          ''', (idx, name))
+        except sqlite3.IntegrityError:
+          pass
+
+      # Table box_entities
+      c.execute('''
+          INSERT INTO box_entities (box_id, entity_id)
+          VALUES (?, ?);
+      ''', (key['SUITE'], idx))
+
+  def __determine_active_boxes(self):
+    """Checks dataframe to determine if boxes are active."""
+    # Check all of the boxes. Set default box status to inactive and look for
+    # any active ones. Result stored in boxes dictionary and returned.
+    boxes = {}
+    for row in self.__df.iterrows():
+      keys = row[1]
+      # Set defaults to inactive
+      if keys['SUITE'] not in boxes:
+        boxes[keys['SUITE']] = False
+      boxes[keys['SUITE']] |= keys['ACTIVE']
+
+    return boxes
 
 class BapDatabaseCreator(DatabaseCreator):
   """Subclass to handle specific data files."""
