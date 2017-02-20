5e2fcc5dfba975360d8bcf6796ec8ab246f06ea3
Chris Ro <christopher.j.ro@gmail.com>
2017-02-20 02:54:06 -0800

    Add database creation test

--- mail_reader/tests/data_access/tests.py
+++ mail_reader/tests/data_access/tests.py
@@ -1,5 +1,5 @@
 import unittest
-import pandas
+import mail_reader.data_access.database_creator as database_creator
 
 class TestDatabaseCreator(unittest.TestCase):
   def setUp(self):
@@ -7,8 +7,13 @@ class TestDatabaseCreator(unittest.TestCase):
 
   def test_create_database_from_excel_file(self):
     path = './mail_reader/tests/data_access/sample_data.xlsx'
-    creator = DatabaseCreator()
-    db = creator.create_database(path)
+    creator = database_creator.DatabaseCreator()
+    db = creator.create_database_from_excel(path)
+    c = db.cursor()
+    c.execute('SELECT * FROM box_entities;')
+    result = c.fetchall()
+    answer = [(111, 0), (5, 1), (22, 2), (111, 3)]
+    self.assertEqual(result, answer)
 
   def tearDown(self):
     pass
