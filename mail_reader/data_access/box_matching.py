df5da528d1e9b873a4ce16a7c8167d2708071d02
Chris Ro <christopher.j.ro@gmail.com>
2017-02-21 17:32:54 -0800

    Add resolve_conflicts and switch to nested lists

--- mail_reader/data_access/box_matching.py
+++ mail_reader/data_access/box_matching.py
@@ -22,7 +22,6 @@ def _get_box_scores(name_to_match, name_list, box_multiplier):
 
   multiplier = 2  # Score multiplier if matches box_multiplier
   box_scores = {}
-
   for [name, box] in name_list:
     # Evaluate similarity and only add if unique. If not unique, only the 
     # highest score is saved so update the score if new one is higher.
@@ -58,7 +57,8 @@ def _combine_boxes_scores(box_scores):
       temp_scores[box] += box_scores_for_name[box]
 
   # Convert to list so we can sort by final score
-  final_scores = sorted(temp_scores.items(), key=itemgetter(1), reverse=True) 
+  as_list = [[k, v] for (k, v) in temp_scores.items()]
+  final_scores = sorted(as_list, key=itemgetter(1), reverse=True) 
 
   return final_scores
 
@@ -86,16 +86,51 @@ class BoxMatcher(object):
         [[box, score], [box, score], ...]
     """
     box_multipliers = mail_fields.probable_box
+    all_names = mail_fields.addressee_line['all_names']
     scores = []
-    for name in mail_fields.addressee_line['all_names']:
+    for name in all_names:
       possible_names = self.__get_active_names_for_letter(name[0].upper())
       name_score = _get_box_scores(name.upper(), possible_names, 
                                    box_multipliers)
       scores.append(name_score)
 
-    final_score = _combine_boxes_scores(scores)
+    combined_score = _combine_boxes_scores(scores)
+    final_score = self.__resolve_conflicts(combined_score, all_names)
 
     return final_score
+
+  def __resolve_conflicts(self, scores, names):
+    """Resolves cases where top scores are same for multiple boxes.
+
+    Args:
+      scores: Result from _combine_boxes_scores().
+    Returns:
+      Final scores list, with scores increased for top contenders.
+    """
+    # scores is already sorted in desc order, so save the top score and modify
+    # each score until we don't have the top score anymore. 
+    top_score = scores[0][1]
+    idx = 0
+    while scores[idx][1] >= top_score:
+      # For each box with a score that we need to reevaluate, grab the entity
+      # names in that box. Those (entity_names) are individually scored again
+      # against each of the original names that were being checked (names). For
+      # each of the original names, calculate the scores and sum them up based
+      # on the entity_id instead, which consolidates an overall entity name 
+      # into a contributing score. The best of these are then added onto the 
+      # original data and then sorted again.
+      (box, score) = scores[idx]
+      entity_names = self.__get_entity_names_for_box(box)
+      entity_scores = []
+      for name in names:
+        entity_scores.append(_get_box_scores(name, entity_names, []))
+        # best = sorted(entity_scores.items(), key=itemgetter(1), reverse=True)[0]
+      combined_entity_scores = _combine_boxes_scores(entity_scores)
+      best = sorted(combined_entity_scores, key=itemgetter(1), reverse=True)[0]
+      scores[idx][1] += best[1]
+      idx += 1
+
+    return sorted(scores, key=lambda x: x[1], reverse=True)
 
   def set_database_connection(self, db_conn):
     """Sets sqlite3 database connection to use for box matching.
@@ -137,3 +172,36 @@ class BoxMatcher(object):
     ''', (letter+'%',))
 
     return c.fetchall()
+
+  def __get_entity_names_for_box(self, box_number):
+    """Queries db for all entities and their names in box_number.
+
+    Args:
+      box_number: Box number to query.
+    Returns:
+      Nested list of names and associated boxes [['NAME1', 20], ['NAME2', 30]].
+    """
+    c = self.__db_conn.cursor()
+    c.execute('''
+        SELECT n.unique_entity_name, n.entity_id 
+               FROM unique_entity_names as n
+                    INNER JOIN box_entities as b
+                            ON n.entity_id=b.entity_id
+                    INNER JOIN entity_statuses as s
+                            ON s.entity_id=b.entity_id
+              WHERE s.current=1
+                AND b.box_id=?;
+    ''', (box_number,))
+
+    return c.fetchall()
+    # sql_result = c.fetchall()
+
+    # entity_names = {}
+
+    # # Add each entry to the dictionary, creating a new list for new ids
+    # for (entity_id, name) in sql_result:
+    #   if entity_id not in entity_names:
+    #     entity_names[entity_id] = []
+    #   entity_names[entity_id].append(name)
+
+    # return entity_names
